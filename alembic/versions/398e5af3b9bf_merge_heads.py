9e6e551fa78e027098879bc814ae7cd1b650c2c7
EdoBufLit <edoardo.buffa@outlook.it>
2026-03-09 23:45:20 +0100

    Restore missing alembic merge revision

---
+++ alembic/versions/398e5af3b9bf_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 398e5af3b9bf
+Revises: d7e8f9a0b1c2, h3i4j5k6l7m8
+Create Date: 2026-03-09 23:30:00
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "398e5af3b9bf"
+down_revision = ("d7e8f9a0b1c2", "h3i4j5k6l7m8")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
